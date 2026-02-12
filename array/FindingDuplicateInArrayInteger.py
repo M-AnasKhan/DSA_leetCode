3e77dac69f8aa64829b6128a2a37c64236ec9a32
M-AnasKhan <wahmiooooo7@gmail.com>
2026-02-13 01:36:08 +0500

    217 - LeetCode Problem solution

---
+++ array/FindingDuplicateInArrayInteger.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)):
+            if i != len(nums) -1:
+                if (nums[i+1] == nums[i]):
+                    return True
+        return False
+
+
+
+
+
